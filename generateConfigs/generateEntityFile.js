81ef1c103fd22c07dc56be77fb6b67755ee6840d
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2024-03-22 16:04:58 +0000

    generateEntityFiles for mysql

--- generateConfigs/generateEntityFile.js
+++ generateConfigs/generateEntityFile.js
@@ -3,43 +3,59 @@ import path from "path";
 
 async function generateEntityFile(columns, tableName) {
   return new Promise((resolve, reject) => {
+    const lastIndex = columns.length - 1;
     const content = `export default {
   tables: {
     name: '${tableName}',
     database: 'mysql',
     columns: [
       ${columns
-        .map(
-          (column) => `{
-        name: '${column.COLUMN_NAME}',
-        ${
-          column.DATA_TYPE === "varchar"
-            ? `type: "string",`
-            : `type: '${column.DATA_TYPE}',`
-        }
-        ${column.COLUMN_KEY ? "primaryKey: true," : "primaryKey: false,"}
-        ${column.IS_NULLABLE ? "nullable: true," : ""} 
-        ${
-          column.COLUMN_DEFAULT
-            ? `defaultValue: '${column.COLUMN_DEFAULT}',`
-            : "defaultValue: null,"
-        }
-        ${
-          column.DATA_TYPE === "varchar"
-            ? `columnType: "string",`
-            : `columnType: '${column.DATA_TYPE}',`
-        }
-      },`
-        )
+        .map((column, index) => {
+          const isLast = index === lastIndex;
+          if (column.COLUMN_KEY === "MUL") {
+            return `{
+              name: '${column.COLUMN_NAME}',
+              type: "object",
+              primaryKey: false,
+              ${column.IS_NULLABLE ? "nullable: true," : ""}
+              ${
+                column.COLUMN_DEFAULT
+                  ? `defaultValue: '${column.COLUMN_DEFAULT}',`
+                  : "defaultValue: null,"
+              }
+              columnType: "object",
+              extra: '${column.EXTRA}',
+              isObject: true,
+              relationType: "n:1",
+              foreignEntity: '',
+              foreignKey: '${column.COLUMN_NAME}',
+            },`;
+          } else {
+            return `{
+              name: '${column.COLUMN_NAME}',
+              ${
+                column.DATA_TYPE === "varchar"
+                  ? `type: "string",`
+                  : `type: '${column.DATA_TYPE}',`
+              }
+              ${column.COLUMN_KEY ? "primaryKey: true," : "primaryKey: false,"}
+              ${column.IS_NULLABLE ? "nullable: true," : ""} 
+              ${
+                column.COLUMN_DEFAULT
+                  ? `defaultValue: '${column.COLUMN_DEFAULT}',`
+                  : "defaultValue: null,"
+              }
+              ${
+                column.DATA_TYPE === "varchar"
+                  ? `columnType: "string",`
+                  : `columnType: '${column.DATA_TYPE}',`
+              }
+              ${column.EXTRA ? `extra: '${column.EXTRA}',` : "extra: '',"}
+            }${isLast ? "" : ","}`;
+          }
+        })
         .join("\n      ")},
     ],
-    ${
-      columns.backoffice
-        ? `backoffice: {
-      icon: '${columns.backoffice.icon}',
-    },`
-        : ""
-    }
   },
 };`;
 
